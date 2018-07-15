cd3ab39f2f813f9a2e8e5f15601218388b41441a
Sebi Kovacs <sebi.kovacs@gmail.com>
2018-07-15 15:43:36 +0300

    Removes the redundant login page

--- magic-volunteers-front-end/src/index.js
+++ magic-volunteers-front-end/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 
-import LoginPage from "./LoginPage";
 import HomePage from "./pages/homepage.react";
 import RegisterPage from "./pages/register.react";
 
@@ -12,15 +11,14 @@ import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/volunteer" component={VolunteerPage} />
-      <Route path="/coordinator" component={CoordinatorPage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/register" component={RegisterPage} />
-      <Route path="/" component={HomePage} />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <Switch>
+            <Route path="/volunteer" component={VolunteerPage} />
+            <Route path="/coordinator" component={CoordinatorPage} />
+            <Route path="/register" component={RegisterPage} />
+            <Route path="/" component={HomePage} />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 registerServiceWorker();
