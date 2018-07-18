e69e708d94b33fe3d5429f03d4d397c2592c18af
Melania Moldovan <mmoldovan@stylyze.com>
2018-07-19 00:19:25 +0300

    Added x-access-token as allowed header

--- magic-volunteers-back-end/app/index.js
+++ magic-volunteers-back-end/app/index.js
@@ -18,7 +18,10 @@ require( "./models/box" );
 
 app.use( ( req, res, next ) => {
     res.header( "Access-Control-Allow-Origin", "*" );
-    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, x-access-token",
+    );
     next();
 } );
 
